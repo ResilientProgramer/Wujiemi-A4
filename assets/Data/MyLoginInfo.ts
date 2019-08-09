5987973c698e94217eb58a7dc8bcd9c749363d29
ResilientProgramer <52796008+ResilientProgramer@users.noreply.github.com>
2019-08-09 19:23:53 -0400

    Add files via upload

---
+++ assets/Data/MyLoginInfo.ts
@@ -0,0 +1,9 @@
+import {WUJIEMI_CLASS } from '../../app/Wujiemi';
+
+export const WUJIEMI_INFO : WUJIEMI_CLASS = {
+      sid: 991473861,
+      sname: 'JieMing Wu',
+      loginname: 'wujiemi',
+      scampus: 'Trafalgar',
+      atittle: 'Assignment#4'
+}
